051fbb82a435f616abc781cc5651a24572176db3
StrongerLord0 <adan10104334@gmail.com>
2023-11-02 11:34:20 -0700

    Changed to Express

---
+++ .history/index_20231102111242.js
@@ -0,0 +1,63 @@
+const fastify = require('fastify')();
+const mysql = require('mysql2');
+const fs = require('fs');
+require('dotenv').config(); 
+
+const database = process.env.DATABASE;
+const username = process.env.DBUSERNAME;
+const host = process.env.DBHOST;
+const password = process.env.DBPASSWORD;
+const port = process.env.PORT;
+
+const connection = mysql.createConnection({
+    host: host,
+    user: username,
+    password: password,
+    database: database,
+    port: 3306,
+    ssl: {
+        ca: fs.readFileSync('./cacert-2023-08-22.pem'),
+    }
+  });
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error al conectar a la base de datos:', err);
+        process.exit(1);
+    }
+    console.log('Conexión a la base de datos establecida');
+})
+
+fastify.get('/getPlants/', (request, reply) => {
+    connection.query('SELECT NombreComun, ImagenBase64 FROM Plantas', (error, results) => {
+        if (error) {
+          console.error('Error al obtener datos de las plantas:', error);
+          reply.code(500).send({ error: 'Error al obtener datos de las plantas' });
+        } else {
+          // Envía los datos de las plantas como respuesta
+          reply.send(results);
+        }
+    });
+});
+
+fastify.get('/getPlant/:id', (request, reply) => {
+  const plantId = request.params.id;
+  connection.query('SELECT * FROM Plantas WHERE ID = ?', [plantId], (error, results) => {
+      if (error) {
+          console.error('Error al obtener datos de la planta:', error);
+          reply.code(500).send({ error: 'Error al obtener datos de la planta' });
+      } else {
+          // Envía los datos de la planta como respuesta
+          reply.send(results);
+      }
+  });
+});
+
+// Arranca el servidor en el puerto 3000
+fastify.listen(port, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Servidor escuchando en ${address}`);
+});
---
+++ .history/index_20231102111455.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const mysql = require('mysql2');
+const fs = require('fs');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT;
+
+const database = process.env.DATABASE;
+const username = process.env.DBUSERNAME;
+const host = process.env.DBHOST;
+const password = process.env.DBPASSWORD;
+
+const connection = mysql.createConnection({
+    host: host,
+    user: username,
+    password: password,
+    database: database,
+    port: 3306,
+    ssl: {
+        ca: fs.readFileSync('./cacert-2023-08-22.pem'),
+    }
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error al conectar a la base de datos:', err);
+        process.exit(1);
+    }
+    console.log('Conexión a la base de datos establecida');
+});
+
+app.get('/getPlants/', (req, res) => {
+    connection.query('SELECT NombreComun, ImagenBase64 FROM Plantas', (error, results) => {
+        if (error) {
+            console.error('Error al obtener datos de las plantas:', error);
+            res.status(500).send({ error: 'Error al obtener datos de las plantas' });
+        } else {
+            res.send(results);
+        }
+    });
+});
+
+app.get('/getPlant/:id', (req, res) => {
+    const plantId = req.params.id;
+    connection.query('SELECT * FROM Plantas WHERE ID = ?', [plantId], (error, results) => {
+        if (error) {
+            console.error('Error al obtener datos de la planta:', error);
+            res.status(500).send({ error: 'Error al obtener datos de la planta' });
+        } else {
+            res.send(results);
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando en el puerto ${port}`);
+});
